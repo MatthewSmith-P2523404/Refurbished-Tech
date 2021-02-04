09422db2f94ec33636f1454ca8bb730c159c8b88
Matthew Smith <P2523404@my365.dmu.ac.uk>
2021-02-04 13:53:16 +0000

    Added the properties based off of the table design for staff management

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public Int32 StaffID { get; set; }
+        public string StaffName { get; set; }
+        public string StaffAddress { get; set; }
+        public DateTime StartDate { get; set; }
+        public double Salary { get; set; }
+        public bool Manager { get; set; }
     }
 }
