6ba1444c554420377f5f551add070a20173d4cdc
Ellis Smith <P2525485@my365.dmu.ac.uk>
2021-02-05 02:04:24 +0000

    Added test class, class & properties for stock management system.

--- Testing4/Properties/AssemblyInfo.cs
+++ Testing4/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Testing4")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Testing Stock")]
+[assembly: AssemblyDescription("Tests related to the stock management system.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Testing4")]
+[assembly: AssemblyCompany("Refurbished Tech Store")]
+[assembly: AssemblyProduct("Stock System Test")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
