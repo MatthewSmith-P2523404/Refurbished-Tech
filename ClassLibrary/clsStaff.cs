56fa2d35b69be55dbdbb0109fd9b7bcdbabebabc
Matthew Smith <P2523404@my365.dmu.ac.uk>
2021-02-15 16:49:18 +0000

    Added a find method for all of the Staff table properties

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,11 +4,90 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
-        public Int32 StaffID { get; set; }
-        public string StaffName { get; set; }
-        public string StaffAddress { get; set; }
-        public DateTime StartDate { get; set; }
-        public double Salary { get; set; }
-        public bool Manager { get; set; }
+        private Int32 mStaffID;
+        private String mStaffName;
+        private String mStaffAddress;
+        private DateTime mStartDate;
+        private Double mSalary;
+        private Boolean mManager;
+
+
+        public Int32 StaffID
+        {
+            get
+            {
+                return mStaffID;
+            }
+            set
+            {
+                mStaffID = value;
+            }
+        }
+        public string StaffName
+        {
+            get
+            {
+                return mStaffName;
+            }
+            set
+            {
+                mStaffName = value;
+            }
+        }
+        public string StaffAddress
+        {
+            get
+            {
+                return mStaffAddress;
+            }
+            set
+            {
+                mStaffAddress = value;
+            }
+        }
+        public DateTime StartDate
+        {
+            get
+            {
+                return mStartDate;
+            }
+            set
+            {
+                mStartDate = value;
+            }
+        }
+        public double Salary
+        {
+            get
+            {
+                return mSalary;
+            }
+            set
+            {
+                mSalary = value;
+            }
+        }
+        public bool Manager
+        {
+            get
+            {
+                return mManager;
+            }
+            set
+            {
+                mManager = value;
+            }
+        }
+
+        public bool Find(int staffID)
+        {
+            mStaffID = 21;
+            mStaffName = "Matthew Smith";
+            mStaffAddress = "A road";
+            mStartDate = Convert.ToDateTime("07/03/2020");
+            mSalary = 1400.50;
+            mManager = true;
+            return true;
+        }
     }
 }
